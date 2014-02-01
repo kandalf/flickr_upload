9d324e0e8d718e690fb1032d195e60e0e225c9f6
Leonardo Mateo <leonardomateo@gmail.com>
2014-02-01 14:36:45 -0300

    Adds ability to resume interrupted set upload

--- lib/flickr_syncd/flickr_account.rb
+++ lib/flickr_syncd/flickr_account.rb
@@ -17,24 +17,28 @@ module FlickrSyncd
     def upload_to_set(media_files, set_id = nil)
       uploaded_files = {}
 
-      if set_id.nil? || set_id.empty?
+      if media_files.any? && (set_id.nil? || set_id.empty?)
         file_path                = media_files.shift
         photo_id                 = flickr.upload_photo file_path, :is_public => 0
         uploaded_files[photo_id] = file_path
-        set_title                = File.basename(File.dirname(media_files.first))
+        set_title                = File.basename(File.dirname(file_path))
         photoset                 = flickr.photosets.create(:title => set_title,
                                                            :primary_photo_id => photo_id)
+        set_id = photoset.id
+        FlickrSyncd::Set.create(:id => set_id, :name => set_title)
       end
 
       media_files.each do |file_path|
         photo_id = flickr.upload_photo file_path, :is_public => 0
         uploaded_files[photo_id] = file_path
-        puts "SET ID: #{photoset.id} NAME: #{set_title}"
+        puts "SET ID: #{set_id} NAME: #{set_title}"
 
-        flickr.photosets.addPhoto(:photoset_id => photoset.id, :photo_id => photo_id)
+        flickr.photosets.addPhoto(:photoset_id => set_id, :photo_id => photo_id)
+
+        FlickrSyncd::Photo.create(:id => photo_id, :title => file_path, :set_id => set_id, :user_id => FlickrSyncd::Authenticator.user_id)
       end
 
-      {:set_id => photoset.id, :uploaded_files => uploaded_files}
+      {:set_id => set_id, :uploaded_files => uploaded_files}
     end
   end
 end
